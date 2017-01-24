657f36414bf2a9d8a26ce83d622256b83a893595
Ehsan <akaseiryu@gmail.com>
2017-01-24 22:51:28 +0330

    int

---
+++ src/RevisionTrait.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ExA2040\LaravelRevision;
+
+trait RevisionTrait {
+
+  public function revisions()
+  {
+    return $this->hasMany('\ExA2040\LaravelViewCounter\Counter', 'object_id')->where('class_name', snake_case(get_class($this)));
+  }
+
+  public static function bootRevisionTrait()
+  {
+    static::created(function($item){
+      // Index the item
+    });
+  }
+
+  protected static function boot(){
+    parent::boot();
+    static::updated(function ($model) {
+      $changes = $model->isDirty() ? $model->getDirty() : false;
+      if($changes)
+      {
+        foreach($changes as $attr => $value)
+        {
+          //Activity::log("updated product $attr from {$model->getOriginal($attr)} to {$model->$attr}");
+        }
+      }
+
+    });
+
+  }
+
+}
